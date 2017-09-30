3ed9e59f940eec9dc41eb0f31c34846d0a0ef24c
taranrai96 <taranrai96@gmail.com>
2017-09-29 21:23:38 -0700

    Commit 3: Added Espanol language support, Landscape mode, back button
    Signed-off-by: taranrai96 <taranrai96@gmail.com>

--- app/src/main/java/ca/bcit/ass1/chriskwon_taranrai/InputActivity.java
+++ app/src/main/java/ca/bcit/ass1/chriskwon_taranrai/InputActivity.java
@@ -8,6 +8,8 @@ import android.view.View;
 import android.content.Intent;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.support.v7.app.AlertDialog;
+import android.content.DialogInterface;
 
 public class InputActivity extends AppCompatActivity {
 
@@ -19,49 +21,62 @@ public class InputActivity extends AppCompatActivity {
         Button yourButton = (Button) findViewById(R.id.button);
 
         yourButton.setOnClickListener(new View.OnClickListener(){
-            public void onClick(View v){
-                Spinner mySpinner=(Spinner) findViewById(R.id.spinner);
+            public void onClick(View v) {
+                Spinner mySpinner = (Spinner) findViewById(R.id.spinner);
                 String selected = mySpinner.getSelectedItem().toString();
                 String choice1 = "";
                 String choice2 = "";
                 EditText myEditText = (EditText) findViewById(R.id.editText);
-                double enteredValue = Double.parseDouble(myEditText.getText().toString());
-                double result = 0;
-                if(selected.equalsIgnoreCase("Kilometers to Miles")) {
-                    result = enteredValue*0.62137;
-                    choice1 = "Kilometers";
-                    choice2 = "Miles";
-                } else if(selected.equalsIgnoreCase("Miles to Kilometers")) {
-                    result = enteredValue*1.60934;
-                    choice1 = "Miles";
-                    choice2 = "Kilometers";
-                } else if(selected.equalsIgnoreCase("Meters to Yards")) {
-                    result = enteredValue*1.0936;
-                    choice1 = "Meters";
-                    choice2 = "Yards";
-                } else if(selected.equalsIgnoreCase("Yards to Meters")) {
-                    result = enteredValue*0.9144;
-                    choice1 = "Yards";
-                    choice2 = "Meters";
-                } else if(selected.equalsIgnoreCase("Meters to Feet")) {
-                    result = enteredValue*3.28;
-                    choice1 = "Meters";
-                    choice2 = "Feet";
-                } else if(selected.equalsIgnoreCase("Feet to Meters")) {
-                    result = enteredValue*0.3048;
-                    choice1 = "Feet";
-                    choice2 = "Meters";
+                if (myEditText.getText().toString().matches("")) {
+                    AlertDialog alertDialog = new AlertDialog.Builder(InputActivity.this).create();
+                    alertDialog.setTitle(getResources().getString(R.string.alert));
+                    alertDialog.setMessage(getResources().getString(R.string.alert_message));
+                    alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, getResources().getString(R.string.ok),
+                            new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int which) {
+                                    dialog.dismiss();
+                                }
+                            });
+                    alertDialog.show();
                 } else {
+                    double enteredValue = Double.parseDouble(myEditText.getText().toString());
+                    double result = 0;
+                    if (selected.equalsIgnoreCase(getResources().getString(R.string.kilo_to_miles))) {
+                        result = enteredValue * 0.62137;
+                        choice1 = getResources().getString(R.string.kilometers);
+                        choice2 = getResources().getString(R.string.miles);
+                    } else if (selected.equalsIgnoreCase(getResources().getString(R.string.miles_to_kilo))) {
+                        result = enteredValue * 1.60934;
+                        choice1 = getResources().getString(R.string.miles);
+                        choice2 = getResources().getString(R.string.kilometers);
+                    } else if (selected.equalsIgnoreCase(getResources().getString(R.string.meters_to_yards))) {
+                        result = enteredValue * 1.0936;
+                        choice1 = getResources().getString(R.string.meters);
+                        choice2 = getResources().getString(R.string.yards);
+                    } else if (selected.equalsIgnoreCase(getResources().getString(R.string.yards_to_meters))) {
+                        result = enteredValue * 0.9144;
+                        choice1 = getResources().getString(R.string.yards);
+                        choice2 = getResources().getString(R.string.meters);
+                    } else if (selected.equalsIgnoreCase(getResources().getString(R.string.meters_to_feet))) {
+                        result = enteredValue * 3.28;
+                        choice1 = getResources().getString(R.string.meters);
+                        choice2 = getResources().getString(R.string.feet);
+                    } else if (selected.equalsIgnoreCase(getResources().getString(R.string.feet_to_meters))) {
+                        result = enteredValue * 0.3048;
+                        choice1 = getResources().getString(R.string.feet);
+                        choice2 = getResources().getString(R.string.meters);
+                    } else {
 
+                    }
+                    Bundle bundle = new Bundle();
+                    bundle.putDouble("convertedValue", result);
+                    bundle.putString("choice1", choice1);
+                    bundle.putString("choice2", choice2);
+                    bundle.putDouble("enteredValue", enteredValue);
+                    Intent i = new Intent(InputActivity.this, OutputActivity.class);
+                    i.putExtras(bundle);
+                    startActivity(i);
                 }
-                Bundle bundle = new Bundle();
-                bundle.putDouble("convertedValue",result);
-                bundle.putString("choice1",choice1);
-                bundle.putString("choice2",choice2);
-                bundle.putDouble("enteredValue",enteredValue);
-                Intent i = new Intent(InputActivity.this, OutputActivity.class);
-                i.putExtras(bundle);
-                startActivity(i);
             }
         });
     }
--- app/src/main/java/ca/bcit/ass1/chriskwon_taranrai/OutputActivity.java
+++ app/src/main/java/ca/bcit/ass1/chriskwon_taranrai/OutputActivity.java
@@ -2,6 +2,8 @@ package ca.bcit.ass1.chriskwon_taranrai;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.view.MenuItem;
 import android.content.Intent;
@@ -15,16 +17,25 @@ public class OutputActivity extends AppCompatActivity {
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
+        Button backButton = (Button) findViewById(R.id.button2);
+
+        backButton.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v) {
+                Intent i = new Intent(OutputActivity.this, InputActivity.class);
+                startActivity(i);
+            }
+        });
+
 
         Bundle bundle = getIntent().getExtras();
         double convertedValue = bundle.getDouble("convertedValue");
         String choice1 = bundle.getString("choice1");
         String choice2 = bundle.getString("choice2");
         double enteredValue = bundle.getDouble("enteredValue");
-        TextView tv = (TextView) findViewById(R.id.textView3);
-        tv.setText(enteredValue + " " + choice1 + " converts to " + String.format("%.2f", convertedValue) + " " + choice2);
+        TextView tv = (TextView) findViewById(R.id.textView);
+        tv.setText(String.format("%.2f", enteredValue) + " " + choice1 + " " + getResources().getString(R.string.converts_to) + " " + String.format("%.2f", convertedValue) + " " + choice2);
     }
-
+    //NOTE: note that in most parts where Español is used, the decimal separator is a comma (source: https://spanish.stackexchange.com/questions/4159/how-to-read-a-decimal-number-in-spanish)
     public boolean onOptionsItemSelected(MenuItem item) {
         Intent myIntent = new Intent(getApplicationContext(), InputActivity.class);
         startActivityForResult(myIntent, 0);
